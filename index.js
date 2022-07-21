ba38e1f66fe7dba99c4b51a2183d081505bfd6d7
willisonpinto <105048864+willisonpinto@users.noreply.github.com>
2022-07-21 10:45:45 -0400

    Mongo Reativado e string de conexão atualizada

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
-const url = "mongodb://localhost:27017";
+//const url = "mongodb://localhost:27017";
+const url = "mongodb+srv://willison:kWOTjli7VZfkImK9@cluster0.wbcsunn.mongodb.net";
 const dbName = "ocean_bancodados_19_07_2022";
 
 async function main() {
-  /*
+  
   console.log("Conectando ao banco de dados...");
 
   const client = await MongoClient.connect(url);
@@ -13,7 +14,7 @@ async function main() {
   const collection = db.collection("herois");
 
   console.log("Banco de dados conectado com sucesso!");
-  */
+  
 
   // Aplicação Backend com Express
 
